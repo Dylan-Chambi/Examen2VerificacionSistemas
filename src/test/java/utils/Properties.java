ff9c88a1b746e8889d533a785adde0ce66a395eb
Dylan-Chambi <dylanchambifi@gmail.com>
2023-06-14 16:39:08 -0400

    Adding code template estructure for test

---
+++ src/test/java/utils/Properties.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Properties {
+
+    public static String pageURL = "";
+    public static String pageUser = "dylan@gmail.com";
+    public static String pagePassword = "dylan123";
+
+    public static String apiHost = "";
+
+    public static String apiUser = "dylan@gmail.com";
+
+    public static String apiPassword = "dylan123";
+
+}
